a763130d001f6c789f588638e25e714c0b2c9690
Maciej Treder <treder.mc@gmail.com>
2022-12-18 13:11:38 +0100

    first

---
+++ Statistics.js
@@ -0,0 +1,26 @@
+class Statistics {
+    constructor() {
+     this.gameResults = [];
+   
+    }
+   
+    addGameToStatistics(win, bid) {
+     let gameResult = {
+      win,
+      bid
+     }
+     // console.log(gameResult);
+     this.gameResults.push(gameResult)
+    }
+   
+    showGameStatistics() {
+     let games = this.gameResults.length;
+     let wins = this.gameResults.filter(result => result.win).length;
+     let losses = this.gameResults.filter(result => !result.win).length
+     // console.log(games, wins, losses);
+     return [games, wins, losses]
+    }
+   
+   }
+   
+   // const stats = new Statistics()
